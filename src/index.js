8b54d1cc62d8c724783e4c4b591f39eb8bfc0859
Marcus Vinicius Abdalla <93200321+MarcusViniciusAbdalla@users.noreply.github.com>
2022-10-11 16:25:32 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
+const { request } = require("express");
 const express = require("express");
 
 const app = express();
+
+app.post("/account", (request , response) => {
+  const {cpf , name } = request.body;
+});
 
 app.listen(3333);
