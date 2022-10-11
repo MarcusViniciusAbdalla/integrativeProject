075d45ec92dbc5f427e25b598ccf3db86c19f4fb
Marcus Vinicius Abdalla <93200321+MarcusViniciusAbdalla@users.noreply.github.com>
2022-10-10 21:54:39 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,8 +4,19 @@ const express = require('express');
 
 const app = express();
 
-app.get("/", (request , response) => {
-  return response.json({message: "Hello World! Node.js "})
+/* GET - Buscar uma informação dentro do servidor
+   POST - Inserir uma informação no servidor
+   PUT - Alterar uma informação no servidor
+   PATCH - Alterar uma informação específica
+   DELETE - Deletar uma informação no servidor
+*/
+
+app.get("/courses", (request , response) => {
+  return response.json(["Curso 1" , "Curso 2 " , "Curso 3"])
+});
+
+app.post("/courses", (request , response) => {
+  return response.json(["Curso 1" , "Curso 2 " , "Curso 3" , "Curso 4"])
 });
 
 app.listen(3333);
